7471cc00af47836846a94ffb3c2095d153d706fc
taguchi.keita <taguchi.keita@alhinc.jp>
2025-11-05 11:05:38 +0900

    first commit

---
+++ src/main/java/com/example/NYA_calculation/NyaCalculationApplication.java
@@ -0,0 +1,13 @@
+package com.example.NYA_calculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyaCalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyaCalculationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NYA_calculation/NyaCalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NYA_calculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyaCalculationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
