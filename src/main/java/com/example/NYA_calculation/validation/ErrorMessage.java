b9e667e59a79984be8a1dbd80c8505968c20b120
taguchi.keita <taguchi.keita@alhinc.jp>
2025-11-05 11:38:45 +0900

    エラーメッセージ

--- src/main/java/com/example/NYA_calculation/validation/ErrorMessage.java
+++ src/main/java/com/example/NYA_calculation/validation/ErrorMessage.java
@@ -2,31 +2,29 @@ package com.example.NYA_calculation.validation;
 
 public class ErrorMessage {
 
-    public static final String E0001 = "アカウントを入力してください";
+    public static final String E0001 = "社員番号を入力してください";
     public static final String E0002 = "パスワードを入力してください";
     public static final String E0003 = "ログインに失敗しました";
-    public static final String E0004 = "予約日を入力してください";
-    public static final String E0005 = "予約時間を入力してください";
-    public static final String E0006 = "予約人数を入力してください";
-    public static final String E0007 = "選択された日付は予約できません";
-    public static final String E0008 = "0人での予約はできません";
-    public static final String E0009 = "定員を超えています";
-    public static final String E0010 = "無効なアクセスです";
-    public static final String E0011 = "不正なパラメータが入力されました";
-    public static final String E0012 = "氏名を入力してください";
-    public static final String E0013 = "アカウントは10文字以下で入力してください";
-    public static final String E0014 = "パスワードは半角文字かつ6文字以上20文字以下で入力してください";
-    public static final String E0015 = "氏名は10文字以下で入力してください";
-    public static final String E0016 = "アカウントが重複しています";
-    public static final String E0017 = "パスワードと確認用パスワードが一致しません";
-    public static final String E0018 = "店舗名を入力してください";
-    public static final String E0019 = "電話番号を入力してください";
-    public static final String E0020 = "住所を入力してください";
-    public static final String E0021 = "エリアを選択してください";
-    public static final String E0022 = "ジャンルを選択してください";
-    public static final String E0023 = "説明を入力してください";
-    public static final String E0024 = "営業開始時間を入力してください";
-    public static final String E0025 = "営業終了時間を入力してください";
+    public static final String E0004 = "承認者を選択してください";
+    public static final String E0005 = "パスワードは半角文字かつ6文字以上20文字以下で入力してください";
+    public static final String E0006 = "パスワードと確認用パスワードが一致しません";
+    public static final String E0007 = "日付を選択してください";
+    public static final String E0008 = "申請理由を選択してください";
+    public static final String E0009 = "金額を入力してください";
+    public static final String E0010 = "交通機関を選択してください";
+    public static final String E0011 = "金額は半角数字で入力してください";
+    public static final String E0012 = "備考は50文字以下で入力してください";
+    public static final String E0013 = "不正なパラメータが入力されました";
+    public static final String E0014 = "ファイルを選択してください";
+    public static final String E0015 = "氏名を入力してください";
+    public static final String E0016 = "部署を選択してください";
+    public static final String E0017 = "権限を選択してください";
+    public static final String E0018 = "社員番号は半角数字7桁で入力してください";
+    public static final String E0019 = "氏名は10文字以下で入力してください";
+    public static final String E0020 = "社員番号が重複しています";
+    public static final String E0021 = "ログインしてください";
+    public static final String E0022 = "無効なアクセスです";
+    public static final String E0023 = "ファイルの形式が不正です";
 
     // インスタンス化防止
     private ErrorMessage() {
