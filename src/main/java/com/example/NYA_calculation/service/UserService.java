7ce72c0bfcd870e6cce5606d76805d9520fcdf4d
taguchi.keita <taguchi.keita@alhinc.jp>
2025-11-05 15:19:06 +0900

    commit

--- src/main/java/com/example/NYA_calculation/service/UserService.java
+++ src/main/java/com/example/NYA_calculation/service/UserService.java
@@ -1,10 +1,17 @@
 package com.example.NYA_calculation.service;
 
 import com.example.NYA_calculation.controller.form.UserForm;
+import com.example.NYA_calculation.mapper.userMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    public UserForm findById(Integer integer) {
+
+    @Autowired
+    userMapper userMapper;
+
+    public UserForm findById(Integer id) {
+        return userMapper.findById(id);
     }
 }
